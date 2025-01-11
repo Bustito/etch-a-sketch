62ada16d6c09ac5adfa02746c822e8af6bb80d03
Martin Bustos <bustos.martin@outlook.com>
2025-01-11 18:35:40 -0300

    Make grid columns/row expandable

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const gridHeight = 16;
+const gridWidth = 16;
+
+const gridElementStyle = document.createElement("style");
+gridElementStyle.type = "text/css";
+gridElementStyle.innerHTML = 
+`.container-element { width: ${100/gridWidth}%; height: ${100/gridHeight}% }`;
+document.getElementsByTagName("head")[0].appendChild(gridElementStyle);
+
 const container = document.querySelector("#divContainer");
 
-for(let i = 0; i < 256; i++) {
-    const gridElement = document.createElement("div");
-    gridElement.classList.add("container-element");
-    container.appendChild(gridElement);
+for(let i = 0; i < gridHeight; i++) {
+    for(let j = 0; j < gridWidth; j++) {
+	const gridElement = document.createElement("div");
+	gridElement.classList.add("container-element");
+    	container.appendChild(gridElement);
+    }
 }
 

f976778a079bbd8a5af92f482d230ea3959a7464
Martin Bustos <bustos.martin@outlook.com>
2025-01-11 19:18:58 -0300

    Implement moddifiable columns and rows

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const gridHeight = 16;
-const gridWidth = 16;
+let gridRows = 16;
+let gridCols = 16;
 
 const gridElementStyle = document.createElement("style");
 gridElementStyle.type = "text/css";
 gridElementStyle.innerHTML = 
-`.container-element { width: ${100/gridWidth}%; height: ${100/gridHeight}% }`;
+`.container-element { width: ${100/gridCols}%; height: ${100/gridRows}% }`;
 document.getElementsByTagName("head")[0].appendChild(gridElementStyle);
 
 const container = document.querySelector("#divContainer");
+const sizeButton = document.querySelector("#sizeButton");
 
-for(let i = 0; i < gridHeight; i++) {
-    for(let j = 0; j < gridWidth; j++) {
+for(let i = 0; i < gridRows; i++) {
+    for(let j = 0; j < gridCols; j++) {
 	const gridElement = document.createElement("div");
 	gridElement.classList.add("container-element");
     	container.appendChild(gridElement);
@@ -20,4 +21,15 @@ for(let i = 0; i < gridHeight; i++) {
 container.addEventListener("mouseover", (event) => {
     const hoverDiv = event.target;
     hoverDiv.classList.add("paintedElement");
+});
+
+sizeButton.addEventListener("click", () => {
+    gridRows = prompt("Insert number of rows");
+    gridCols = prompt("Insert number of cols");
+    gridElementStyle.innerHTML = 
+    `.container-element { width: ${100/gridCols}%; height: ${100/gridRows}% }`;
+    const gridElements = container.querySelectorAll(".container-element");
+    for(const elem of gridElements) {
+	elem.classList.remove("paintedElement");
+    }
 });
